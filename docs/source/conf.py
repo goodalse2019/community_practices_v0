a3a2a25dc9647caa8b48aebe04934a3331afde02
Sarah Goodale <60117796+goodalse2019@users.noreply.github.com>
2023-12-06 00:14:54 -0600

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Physiopy'
+copyright = '2023-" + datetime.today().strftime("%Y") + ", physiopy developers'
+author = 'physiopy developers'
 
 release = '0.1'
 version = '0.1.0'
